7227fe00ea3d1a9675236080f61cc1e0a75865c7
Adam Hamilton-Sutherland <55899344+adamHamland@users.noreply.github.com>
2020-11-05 21:47:35 -0800

    Uploaded project to github

---
+++ README.md
@@ -0,0 +1,89 @@
+# Summary
+
+### Pokemon Insurgence GUI automation with python
+
+This is a GUI automation script build for finding a shiny starter pokemon in the 
+community made pokemon insurgence video game.
+
+Currently This only works with 1080p monitors since your mouse is moved and screen shots are taken at 
+predefined locations based on a hight and width of a 1080p monitor.
+
+This script forces certain mouse and keyboard inputs. You can not use your computer and this script at 
+the same time and expect it to work properly.
+
+This has not been tested on Mac or Linux but it should work.
+
+# Installation
+YOU MUST HAVE PYTHON 3 INSTALLED
+
+### Install dependencies 
+```pip install -r requirements.txt```
+
+or
+
+```python -m pip install -r requirements.txt```
+
+or
+
+```py -m pip install -r requirements.txt```
+
+# Use
+
+### Setup before using the script
+
+By default this script assumes you have your in game text speed set to insane and your game speed
+set to max (press m on the keyboard to cycle through the pokemon game speeds. Red is fastest.)
+
+Make sure size of window is Normal.
+
+Make sure you have saved the game in front of the starter pokeball you want
+and that you have saved the game after setting up your game speed settings and window size settings.
+
+### Using the script
+
+Start Pokemon Insurgence Game.
+
+Go through the start menu and load your save file so you are in front of the pokeball you want.
+
+Move game window to upper left corner of your screen. It wont snap there but try to line it up.
+
+Start script and wait.  
+
+### Run:  
+```python shiny_starter_finder.py```
+
+or
+
+```py shiny_starter_finder.py```
+
+### Run with debugging:  
+
+LOTS OF IMAGES WILL BE WRITTEN TO YOUR DIRECTORY. 
+
+21 to be exact. 1 of those images will be overwritten constantly throughout the time the script is running.
+
+```python shiny_starter_finder.py debug```
+
+or
+
+```py shiny_starter_finder.py debug```
+
+If debug mode is on, image files will be saved in the directory and these can be used to trouble shoot issues.
+
+
+This script will use keyboard inputs to select the pokemon and then screen shot the sprite after the pokemon 
+appears behind your character. The initial phase of the script will capture a screen shot of the pokemon that 
+it will be compared over and over again to new screen shots of the pokemon after the game restarts. 
+If the screen shots are different, then something in the image has changed, you might have a shiny. YAY.
+
+In your terminal press the key combination ctrl + c (ctrl and c) to quit the script.
+
+# Advanced Use
+
+If you want to play around with different game speeds you can change the speed 
+variable in the ```if __name__ == "__main__"``` function.
+
+If you have a different sized monitor, you can adjust the location of the screen 
+shot by changing the monitor variables int the screen_shot function. Run in debug mode
+so the screen shots will be written out to a file so you can view them. Make sure the 
+pokemon is in the image.
---
+++ shiny_starter_finder.py
@@ -0,0 +1,174 @@
+import time
+import sys
+
+import numpy as np
+
+import pynput
+
+import mss
+import mss.tools
+
+from pynput.mouse import Button, Controller
+from pynput import keyboard
+
+from PIL import Image
+
+mouse = pynput.mouse.Controller()
+
+def select_pokemon(speed):
+    time.sleep(.5)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(2/speed)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1/speed)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(.5)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(.5)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1/speed)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1/speed)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1/speed)
+    keyboard.Controller().press(keyboard.Key.esc)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.esc)
+
+    print("Selected Pokemon.")
+
+def skip_through_startup():
+    time.sleep(2)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+
+    time.sleep(1)
+    keyboard.Controller().press(keyboard.Key.enter)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.enter)
+    #to here---------
+
+    print("Started Game.")
+
+    time.sleep(.5)
+
+def screen_shot(debug, fname):
+    with mss.mss() as sct:
+        #The region to capture
+        monitor = {"top": 170, "left": 170, "width": 160, "height": 135}
+        output = fname + ".png"
+
+        #Screen shot
+        sct_img = sct.grab(monitor)
+
+        if debug == True:
+            #Save the picture to a file
+            mss.tools.to_png(sct_img.rgb, sct_img.size, output=output)
+
+        img = Image.frombytes('RGB', sct_img.size, sct_img.rgb)
+        img_arr = np.array(img)
+        return img_arr
+
+
+def setup(debug, speed):
+    print("Setting up.")
+    #Move mouse to game window and select window so it reads the keyboard
+    mouse.position=(50,50)
+    time.sleep(.2)
+    mouse.press(Button.left)
+    time.sleep(.2)
+    mouse.release(Button.left)
+
+    #select the pokemon 
+    select_pokemon(speed)
+
+    #Get screen shots for later comparison
+    #We need multiple screen shots because of small variants in the timings
+    # means the sometimes the pokemon sprite is in a different animation
+    # state. If the initial image does not match the new one
+    # taken when the find_shiny loop is running, it will stop the loop
+    # in case there is a shiny pokemon.
+    initial_img_arrs = list()
+    for i in range(0, 20):
+        initial_img_arrs.append(screen_shot(debug, "initial" + str(i)))
+
+    #Restart the game
+    time.sleep(1)
+    keyboard.Controller().press(keyboard.Key.f12)
+    time.sleep(.3)
+    keyboard.Controller().release(keyboard.Key.f12)
+
+    return initial_img_arrs
+
+def find_shiny(initial_img_arrs, debug, speed):
+    shiny_found = False
+
+    while shiny_found == False:
+        skip_through_startup()
+
+        select_pokemon(speed)
+
+        img_arr = screen_shot(debug, "check_shiny")
+
+        print('Comparing to original to see if pokemon is shiny.')
+        #Loop through the initial img set and see if any of them match the current screen shot
+        #If there is a match found, then there is no shiny
+        img_found = False
+        for i in initial_img_arrs:
+            if np.array_equal(i, img_arr, False):
+                img_found = True
+        
+        #No matching image found, then there might be a shiny.
+        if img_found == False:
+            print("Possible shiny found, stoping script.")
+            shiny_found = True
+        else:
+            #Restart the game
+            time.sleep(.5)
+            keyboard.Controller().press(keyboard.Key.f12)
+            time.sleep(.3)
+            keyboard.Controller().release(keyboard.Key.f12)
+
+if __name__ == "__main__":
+    debug = False
+    speed = 5
+    
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "debug":
+            debug = True
+            print("Running in debug mode...")
+    
+    initial_img_arrs = setup(debug, speed)
+    find_shiny(initial_img_arrs, debug, speed)
